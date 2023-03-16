00dd6b2c8ee2b328ee9f4148cadbf41896537607
Israel Oliveira <ifelixl.oliveira@gmail.com>
2023-03-16 15:23:04 -0300

    alterando estilo da pagina

--- public/js/port.js
+++ public/js/port.js
@@ -1,31 +1,31 @@
-const sessaoProjetos = document.getElementById('projetos-escondidos')
-const botao1 = document.getElementById('projetos-botao-mais')
-const botao2 = document.getElementById('projetos-botao-menos')
 const menu = document.getElementById('menu')
 const linha1 = document.getElementById('linha1')
 const linha2 = document.getElementById('linha2')
 const linha3 = document.getElementById('linha3')
 const nav = document.getElementById('nav')
+const perfil_div = document.getElementById('sobre-div');
+const conteudo = document.getElementById('conteudo');
+const rodape = document.getElementById('rodape');
 
+var largura_tela
+var pad_valor
+var pad
 
-function mostrarProjetos() {
-        sessaoProjetos.style.display = 'flex';
-        console.log('mostrou mais')
-        botao1.style.display = 'none'
-        botao2.style.display = 'block'
-}
-
-function esconderProjetos() {
-        sessaoProjetos.style.display = 'none';
-        console.log('mostrou menos')
-        botao2.style.display = 'none'
-        botao1.style.display = 'block'
+function Padding_perfil () {
+        largura_tela = window.innerWidth;
+        if (largura_tela > 800) {
+                pad_valor = (largura_tela - 800) / 60;
+                pad = '0 ' + pad_valor + '%';
+                perfil_div.style.padding = pad;
+        }
 }
 
 function mostrarNav () {
         nav.classList.toggle('cabecalho-abas-lateral')
-        console.log ('apareceu a navegação lateral')
+        // console.log ('apareceu a navegação lateral')
         menu.removeEventListener ('click', mostrarNav)
+        conteudo.addEventListener ('click', esconderNav)
+        rodape.addEventListener ('click', esconderNav)
         menu.addEventListener ('click', esconderNav)
         linha1.setAttribute ('class', 'cabecalho-menu-linhaX1')
         linha2.setAttribute ('class', 'cabecalho-menu-linhaX2')
@@ -34,7 +34,9 @@ function mostrarNav () {
 
 function esconderNav () {
         nav.classList.toggle('cabecalho-abas-lateral')
-        console.log ('desapareceu a navegação lateral')
+        // console.log ('desapareceu a navegação lateral')
+        conteudo.removeEventListener ('click', esconderNav)
+        rodape.removeEventListener ('click', esconderNav)
         menu.removeEventListener ('click', esconderNav)
         menu.addEventListener ('click', mostrarNav)
         linha1.setAttribute ('class', 'cabecalho-menu-linha')
@@ -42,9 +44,8 @@ function esconderNav () {
         linha3.setAttribute ('class', 'cabecalho-menu-linha')
 }
 
-
-botao1.onclick = mostrarProjetos
-botao2.onclick = esconderProjetos
 menu.addEventListener ('click', mostrarNav)
+
+setInterval(Padding_perfil, 1)
 
 
